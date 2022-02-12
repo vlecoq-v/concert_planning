e733e897889d5d9f12c475b3bb40fe0df84fefd4
Valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2022-02-12 18:56:02 +0100

    better algo sorted

--- find_sum.py
+++ find_sum.py
@@ -5,32 +5,28 @@ class IllegalArgumentError(ValueError):
     pass
 
 
-def find_sum(target_sum: int, elems: list[int]) -> Union[list[int], None]:
+def find_sum(target_sum: int, int_list: list[int]) -> Union[list[int], None]:
     """Finds a list of 3 integers within @arg list that have a sun of @arg target_sum.
     """
 
-    elems_size = len(elems)
-    if elems_size < 3:
+    int_list_size = len(int_list)
+    if int_list_size < 3:
         raise IllegalArgumentError
 
-    print(f"size = {elems_size}")
+    int_list.sort()
 
-    first = 0
-    while first < elems_size - 2:
-        print(f"first = {first}")
+    for first in range(0, int_list_size - 2):
         second = first + 1
-        while second < elems_size - 1:
-            print(f"second = {second}")
-            third = second + 1
-            while third < elems_size:
-                print(f"third = {third}, {elems[third]}")
-                if elems[third] == target_sum - (elems[first] + elems[second]):
-                    return [first, second, third]
-                print(target_sum - (elems[first] + elems[second]))
-                third += 1
-            second += 1
-        first += 1
-        print(first < elems_size - 3)
-    print(f"finished {[first, second, third]}")
+        third = int_list_size - 1
+
+        while (second < third):
+            current_sum = int_list[first] + int_list[second] + int_list[third]
+            print(f"iteration : {first}, {second}, {third}")
+            if current_sum == target_sum:
+                return [first, second, third]
+            elif current_sum < target_sum:
+                second += 1
+            else:
+                third -= 1
 
     return
